f3ced5241fbebbcb5456da60a987b7930c5278a9
rakeshkr114 <M1039158@mindtree.com>
2020-09-13 14:47:43 +0530

    Updaed configs

--- src/main/java/com/example/configserver/ConfigServerApplication.java
+++ src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.configserver;
+	package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
